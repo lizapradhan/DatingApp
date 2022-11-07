82d1c3a72dcda19b1b77112ca584d2a73fdb3180
lizapradhan <lizapradhan588@gmail.com>
2022-11-07 22:23:48 +0530

    Initial push

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = (IConfiguration)builder.Configuration;
+builder.Services.AddDbContext<DataContext>(
+    options =>
+    {
+        options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+    });
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
